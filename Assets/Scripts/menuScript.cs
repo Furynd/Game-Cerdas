067138d57f76b0d63d055c09cfbfe338d6d59f94
Furqan Aliyuddien <furqanaliyuddien@gmail.com>
2020-04-23 12:06:16 +0700

    add main menu

---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("menuScript");
+    }
+
+    public void LoadA(string scenename)
+    {
+        Debug.Log("sceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
